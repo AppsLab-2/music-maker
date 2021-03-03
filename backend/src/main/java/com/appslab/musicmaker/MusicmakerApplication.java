fcb5d7ce8c869a18741b6a34765b35b75d1b138a
gachojuraj <mmamma151@gmail.com>
2021-03-03 23:46:39 +0100

    created backend project

---
+++ backend/src/main/java/com/appslab/musicmaker/MusicmakerApplication.java
@@ -0,0 +1,13 @@
+package com.appslab.musicmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicmakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicmakerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/appslab/musicmaker/MusicmakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appslab.musicmaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicmakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
