2e64885e0a218caf4203ecb1a281e999a753e65e
gachojuraj <mmamma151@gmail.com>
2021-03-27 17:22:03 +0100

    added login

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,8 +10,13 @@ import { SongEditorComponent } from './song-editor/song-editor.component';
 import { PatternListComponent } from './pattern-list/pattern-list.component';
 import { SampleListComponent } from './sample-list/sample-list.component';
 import { PatternPropertiesComponent } from './pattern-properties/pattern-properties.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -21,14 +26,20 @@ import { MatButtonModule } from '@angular/material/button';
     SongEditorComponent,
     PatternListComponent,
     SampleListComponent,
-    PatternPropertiesComponent
+    PatternPropertiesComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatIconModule,
     FormsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/user.service.ts
+++ frontend/src/app/user.service.ts
@@ -1,10 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog'
+import { LoginFormComponent } from './login-form/login-form.component';
+import { User } from './User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  public username: String = "testUser1";
+  user: User;
+  isLogged: boolean = false;
 
-  constructor() { }
+  constructor(public dialog: MatDialog, private http: HttpClient) { }
+
+  userClicked(){
+    if (this.isLogged) this.logout();
+    else this.showLoginForm();
+  }
+
+  showLoginForm(incorrect?: any){
+    const dialogRef = this.dialog.open(LoginFormComponent, {
+      width: '400px', data: {incorrect: incorrect}
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result){
+        this.user = result.user;
+        this.isLogged = true;
+      }
+    });
+  }
+
+  logout(){
+    this.isLogged = false;
+    this.http.get<User>("http://localhost:8080/logout");
+  }
 }
